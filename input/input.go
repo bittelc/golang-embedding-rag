a9200c9fc67919416982414c7097a90c6b130dfb
Cole Bittel <cole.bittel@pm.me>
2025-06-13 10:10:11 +0200

    refact to have a use external package
    Signed-off-by: Cole Bittel <cole.bittel@pm.me>

---
+++ input/input.go
@@ -0,0 +1,19 @@
+package input
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func GetUserInput() (string, error) {
+	var userInput string
+	fmt.Print("Text to embed: ")
+	reader := bufio.NewReader(os.Stdin)
+
+	userInput, err := reader.ReadString('\n')
+	if err != nil {
+		return userInput, fmt.Errorf("couldn't create Url for OpenAI endpoint", err)
+	}
+	return userInput, nil
+}
