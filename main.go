d9aa448eb634974b17ee48ef54788f19e37e002a
Cole Bittel <cole.bittel@pm.me>
2025-06-20 22:32:07 +0200

    output to file instead of stdout

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	oai "golang-embedding-rag/openai"
 	"log/slog"
 	"os"
+	"time"
 
 	"github.com/davecgh/go-spew/spew"
 )
@@ -35,7 +36,17 @@ func main() {
 		fmt.Fprintf(os.Stderr, "error with response from embeddings request: %v\n", err)
 		os.Exit(1)
 	}
-	spew.Dump(resp)
+	timestamp := time.Now().Format("20060102.1504")
+	filename := fmt.Sprintf("data/output_%s", timestamp)
+
+	file, err := os.Create(filename)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error creating output file: %v\n", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	spew.Fdump(file, resp)
 
 	slog.Info("Reached end of program")
 }
